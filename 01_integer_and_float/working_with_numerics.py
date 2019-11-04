76aa8ddaafd9a5daa3865cdc45be71d387826f4a
Nazeeruddin <56721552+benefice-bytes@users.noreply.github.com>
2019-11-04 12:44:00 +0530

    working with numerics

---
+++ 01_integer_and_float/working_with_numerics.py
@@ -0,0 +1,94 @@
+#scenario 1
+"""
+num_1 = 10
+print(num_1)
+print(type(num_1))
+"""
+#scenario 2
+"""
+num_1 = 3.2
+print(num_1)
+print(type(num_1))
+"""
+#scenario 3
+"""
+print(3+2)
+print(3-2)
+print(3//2) # floor division
+print(3/2) # division
+print(3%2) # modulus
+print(3 ** 2) # exponent or power
+"""
+#scenario 4
+# order of arithmatical operation
+"""
+print(3 * 2 + 1)
+print(3 * (2 + 1))
+"""
+#scenario 5
+#incrementing
+"""
+num_1 = 10
+num_1 *= 1
+print(num_1)
+num_2 = 10
+num_2 *= 10
+print(num_2)
+"""
+# scenario 6
+# comparision or relational expressions
+"""
+print(3 != 2)
+"""
+# scenario 7
+# type casting to integer
+"""
+num_1 = '100'
+num_2 = '200'
+print(num_1 + num_2)
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+print(num_1 + num_2)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
