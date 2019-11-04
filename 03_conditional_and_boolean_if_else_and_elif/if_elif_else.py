2dc87d1a77f8c81a78fc64485259acf7cdcda513
Nazeeruddin <56721552+benefice-bytes@users.noreply.github.com>
2019-11-04 14:46:06 +0530

    if_elif_else.py uploaded

---
+++ 03_conditional_and_boolean_if_else_and_elif/if_elif_else.py
@@ -0,0 +1,57 @@
+"""
+condition = True
+if condition:
+      print('condtion is true')
+else:
+      print('condition is false')
+"""
+##########
+"""
+name = 'Nagesh'
+age = 18
+if name == 'Satish' or age >= 18:
+      print('valid voter')
+else:
+      print('invalid voter')
+"""
+##############
+"""
+mesg = []
+
+if mesg:
+      print('condtion is true')
+else:
+      print('condition is false')
+"""
+# difference between == and is operator
+
+a = [1,2,3]
+b = [1,2,3]
+print(a == b) # True
+print(id(a)) # ref of a
+print(id(b)) # ref of b
+print(id(a) == id(b)) # compares ref
+print(a is b) # compares ref a and ref b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
